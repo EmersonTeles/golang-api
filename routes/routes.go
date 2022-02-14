f03fba0c407001a06ddb77dab49b43135a0283bf
emersonteles <emersonteles21@gmail.com>
2022-02-13 23:04:49 -0300

    feat: update personality

--- routes/routes.go
+++ routes/routes.go
@@ -14,5 +14,6 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.GetPersonalityById).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CreateNewPersonality).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletePersonality).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.UpdatePersonality).Methods("Put")
 	http.ListenAndServe(":8000", r)
 }
