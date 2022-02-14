5a235e40d168c8c71616bd5a16845f4e7f677dc4
emersonteles <emersonteles21@gmail.com>
2022-02-13 23:27:54 -0300

    feat: content type middleware

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/EmersonTeles/golang-api/controllers"
+	"github.com/EmersonTeles/golang-api/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.AllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.GetPersonalityById).Methods("Get")
